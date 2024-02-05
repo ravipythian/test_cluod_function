505480de36e6636eae1f49158c4ed85fe91d8c31
unknown <rajak@pythian.com>
2024-02-05 21:49:20 +0530

    test

--- function/main.py
+++ function/main.py
@@ -1,2 +1,16 @@
-def greet():
-    print('Hello World!')
+#def greet():
+#    print('Hello World!')
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    # Replace this with the actual data you want to return
+    data = {'message': 'Hello, this is your API response!'}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
